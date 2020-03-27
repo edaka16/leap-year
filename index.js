d0143a7a41dc3608586c977a3373d68f90459efc
edaka16 <48714436+edaka16@users.noreply.github.com>
2020-03-27 16:20:45 +0100

    make changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var today = new Date();
 var currentYear = today.getFullYear();
-var leapyear = new Date("2020-02-29 0:00:00");
+var leapyear = new Date( currentYear + "-02-29 0:00:00");
 
 var year_range = [];
 var new_array = [];
@@ -8,35 +8,31 @@ var new_array = [];
 console.log(today);
 
 if(today <= leapyear){
-  console.log(leap_year_range(currentYear));
-    
-    
-}else{
-    var leapyearwithcurrent = currentYear + 1;
-   console.log(leap_year_range(leapyearwithcurrent))
+    leap_year_range(currentYear);
+    }else{
+        var leapyearwithcurrent = currentYear + 1;
+   leap_year_range(leapyearwithcurrent);
     
 }
 
 function leap_year_range(start) {
-for (var i = start ; i <=start + 39; i++)
-{
-     year_range.push(i);
-}
+    for (var i = start ; i <=start + 39; i++){
+        year_range.push(i);
+    }
 
-year_range.forEach(
-    
-function(year)
-    { 
-    if (test_LeapYear(year)) 
-    new_array.push(year);
-    });
+    year_range.forEach(   
+        function(year){ 
+            if (test_LeapYear(year)) {
+            new_array.push(year);
+        }
+        });
 
-return new_array;
+        return new_array;
     }
 
 function test_LeapYear(year) {
      if ((year % 4 === 0 && year % 100 !== 0) || (year % 100 === 0 && year % 400 === 0)) {
-                return year;
+                return true;
         } else {
                 return false;
         }
@@ -48,5 +44,5 @@ function showYears(){
     })
     document.getElementById('years').innerHTML=
     '<li>' + allYears.join('</li><li>') + '</li>'
-}
+   }
 showYears()
